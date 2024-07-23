34f73405cc76193e1c41138a6561548e8dfab200
Debankur04 <134576718+Debankur04@users.noreply.github.com>
2024-07-23 13:05:01 +0530

    Add files via upload

--- JS/Signin.js
+++ JS/Signin.js
@@ -3,6 +3,11 @@ const signUpBtn = document.getElementById("signUp");
 const fistForm = document.getElementById("form1");
 const secondForm = document.getElementById("form2");
 const container = document.querySelector(".container");
+const user_signup = document.querySelector(".user-signup");
+const email_signup= document.querySelector(".email-signup");
+const password_signup= document.querySelector(".password-signup");
+const email_signin= document.querySelector(".email-signin");
+const password_signin= document.querySelector(".password-signin");
 
 signInBtn.addEventListener("click", () => {
 	container.classList.remove("right-panel-active");
@@ -14,3 +19,306 @@ signUpBtn.addEventListener("click", () => {
 
 fistForm.addEventListener("submit", (e) => e.preventDefault());
 secondForm.addEventListener("submit", (e) => e.preventDefault());
+
+//JS to have database
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAZMFRpHxCY6THKY8oL--o17J-vkPmtG7Q",
+	authDomain: "demandintel-ai.firebaseapp.com",
+	projectId: "demandintel-ai",
+	storageBucket: "demandintel-ai.appspot.com",
+	messagingSenderId: "405515129428",
+	appId: "1:405515129428:web:98e28c85159da0445553c1"
+};
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);   
+  const db = firebase.firestore();
+  const storage = firebase.storage();
+  let timer;
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+
+  
+  today  = new Date();
+  var date = today.getDate() + "/" + (today.getMonth()+1) + "/" + today.getFullYear() ; //gives the  current date to the system
+
+
+document.getElementById("name-containerReg").addEventListener("keyup", (e) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+        globalThis.userNameReg = document.getElementById("name-containerReg").value.toLowerCase();
+        isUser(userNameReg);
+    
+        }, 500);
+        
+        document.getElementById("name-containerReg").value.length == 0 ? document.querySelector(".registerForm > .name-container > .validUserName").style.opacity = "0" : null;
+})
+
+
+document.getElementById("signup_button").addEventListener("click", () => {
+    const regName = document.getElementById("name-containerReg").value;
+    const regEmail = document.getElementById("email-containerReg").value;
+    const regPass = document.getElementById("password-containerReg").value;
+    
+        if(isEmail(regEmail))
+        {
+            if((isPassword(regPass)))
+            {
+                document.querySelector(".loaderMask").style.zIndex = "100";
+                document.querySelector(".loaderMask").style.opacity = "1";
+                //create account of the user
+                firebase.auth().createUserWithEmailAndPassword(regEmail, regPass)
+                .then((userCredential) => {
+                    var user = userCredential.user //contains the user credentials
+                    db.collection('users').doc(regName.toLowerCase().trim()).set({
+                        username : regName.toLowerCase().trim(),
+                        password : regPass,
+                        email : regEmail,
+                        uid : user.uid,
+                        displayName : regName,
+                        dateCreated: date,
+                        region : "null",
+                    })
+                    .then(() => {
+                        //do the needful to bring up the login page
+                        
+                        document.getElementById("name-containerReg").value = "";
+                        document.getElementById("email-containerReg").value = "";
+                        document.getElementById("password-containerReg").value = "";
+                        document.getElementById("confirmPassword-containerReg").value = "";
+
+
+                        document.querySelector(".loaderMask").style.zIndex = "-1";
+                        document.querySelector(".loaderMask").style.opacity = "0";
+                        document.querySelector(".registerForm").style.top = "150%";
+                        document.querySelector(".loginForm").style.top = "50%";
+
+                    })
+                })
+                .catch((error) => {
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    errorMessage == "The email address is already in use by another account." ? typeWriterErrorHTML("errorMessage","The email address is already in use"): typeWriterErrorHTML("errorMessage", errorMessage);
+                    
+                  });
+            }
+            else if(!(isPassword(regPass)))
+            {
+                typeWriterErrorHTML("errorMessage", "Password don't match");
+            }
+        }
+        else if(isEmail(regEmail) == false)
+        {
+            typeWriterErrorHTML("errorMessage", "Invalid EMail Address");
+        }
+    
+})
+
+
+document.getElementById("signin_btn").addEventListener("click", () =>{
+    const loginName = document.getElementById("signinNameinp").value.toLowerCase().trim();
+    const loginPass = document.getElementById("signinPasswordInp").value;
+    document.querySelector(".loaderMask").style.zIndex = "100";
+    document.querySelector(".loaderMask").style.opacity = "1";
+    if(loginName != "")
+    {
+        db.collection("users").doc(loginName).get().then((doc) => {
+            if (doc.exists) {
+                console.log(doc.data());
+                if(doc.data().password != loginPass)
+                {
+                    typeWriterErrorHTML("errorMessageLogin", "Wrong Password");
+                    document.querySelector(".loaderMask").style.zIndex = "-1";
+                    document.querySelector(".loaderMask").style.opacity = "0";
+                }
+                else
+                {   localStorage.setItem("farmerPodUser",loginName);
+                    location.replace("inermediate.html");
+                }
+            }
+            else
+            {
+                typeWriterErrorHTML("errorMessageLogin", "Account Doesn't Exist, Sign Up")
+                document.querySelector(".loaderMask").style.zIndex = "-1";
+                document.querySelector(".loaderMask").style.opacity = "0";
+            }
+        })
+        
+    }
+    else 
+    {
+        typeWriterErrorHTML("errorMessageLogin", "Insufficient Information")
+    }
+    
+})
+
+function isUser(userName)
+{
+    if(userName.trim() != "")
+    {
+        var usrFind = db.collection("users").doc(userName);
+        usrFind.get().then((doc) => {
+            if(doc.exists)
+            {
+                document.querySelector(".registerForm > .name-container > .validUserName").style.opacity = "0"
+                document.getElementById("name-containerReg").style.color = "red";
+                typeWriterErrorHTML("errorMessage", "This Username Exists");
+                return false;
+            }
+            else
+            {
+                document.getElementById("name-containerReg").style.color = "#3b3b3b";
+                document.querySelector(".registerForm > .name-container > .validUserName").style.opacity = "1"
+                return true;
+            }
+        });
+    }
+    else
+    {
+        return false;
+    }
+    if(userName.length < 5)
+    {
+        return false;
+    }
+}
+
+function isEmail(emailAdress){
+
+    emailAdress = document.getElementById("email-containerReg").value;
+    if (emailAdress.match(regex)) 
+    return true;
+    else 
+    {
+        return false; 
+    }
+    
+    }
+    
+    document.getElementById("email-containerReg").addEventListener("input", () => {
+        var emailReg = document.getElementById("email-containerReg").value;
+        if(isEmail(emailReg) == true)
+        {
+        document.getElementById("validEmail").style.opacity = "1";
+        }
+        else
+        {
+            document.getElementById("validEmail").style.opacity = "0";
+    
+        }
+        emailReg.length == 0 ? document.getElementById("validEmail").style.opacity = "0" : null;
+    })
+
+function isPassword(pass)
+{
+    regconfPass = document.getElementById("confirmPassword-containerReg").value;
+    console.log(pass.length)
+    if((pass.length >= 6) && (pass == regconfPass))
+    {   
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+
+}
+
+document.getElementById("password-containerReg").addEventListener("input", () => {
+    regPass = document.getElementById("password-containerReg").value;
+    regconfPass = document.getElementById("confirmPassword-containerReg").value;
+    if(isPassword(regPass))
+    {
+        document.getElementById("validPassword").style.opacity = "0";
+        document.getElementById("validPasswordConf").style.opacity = "0";
+    }
+    else
+    {
+        document.getElementById("validPassword").style.opacity = "1";
+        document.getElementById("validPasswordConf").style.opacity = "1";
+    }
+
+
+})
+
+document.getElementById("confirmPassword-containerReg").addEventListener("input", () => {
+    regPass = document.getElementById("password-containerReg").value;
+    regconfPass = document.getElementById("confirmPassword-containerReg").value;
+    if(isPassword(regPass))
+    {
+        document.getElementById("validPassword").style.opacity = "0";
+        document.getElementById("validPasswordConf").style.opacity = "0";
+    }
+    else
+    {
+        document.getElementById("validPassword").style.opacity = "1";
+        document.getElementById("validPasswordConf").style.opacity = "1";
+    }
+
+
+})
+
+
+
+
+function typeWriterErrorHTML(idOfTextHolder, textToType, speed) {
+    var i = 0;
+    var speed = speed || 25; // Default speed if not provided
+    document.getElementById(idOfTextHolder).innerHTML = "";
+    function type() {
+        if (i < textToType.length) {
+            document.getElementById(idOfTextHolder).innerHTML += textToType.charAt(i);
+            i++;
+            setTimeout(type, speed);
+        }
+         if(i == textToType.length )
+         {
+            setTimeout(() => {
+                document.getElementById(idOfTextHolder).innerHTML = "";
+            }, 1500);
+         }
+    }
+    type(); // Call the function to start the typing effect
+}
+
+function checkUserOnLogin()
+{
+
+        if(localStorage.getItem("farmerPodUser") != null)
+        {
+
+            location.replace("inermediate.html");
+        }
+    
+}
+
+window.onload = e => {
+    checkUserOnLogin();
+}
+
+
+document.getElementById("SeePwd").addEventListener("click", () => {
+    document.getElementById("SeePwd").classList.contains("hidden") ? (document.getElementById("SeePwd").classList.remove("hidden")) (document.getElementById("SeePwd").setAttribute("name", "eye-off")) : (document.getElementById("SeePwd").classList.add("hidden")) (document.getElementById("SeePwd").setAttribute("name", "eye"));
+});
+
+document.getElementById("SeePwdReg").addEventListener("click", () => {
+    document.getElementById("SeePwdReg").classList.contains("hidden") ? (document.getElementById("SeePwdReg").classList.remove("hidden")) (document.getElementById("SeePwdReg").setAttribute("name", "eye-off")) : (document.getElementById("SeePwdReg").classList.add("hidden")) (document.getElementById("SeePwdReg").setAttribute("name", "eye"));
+});
+
+
+document.getElementById("SeePwdConf").addEventListener("click", () => {
+    document.getElementById("SeePwdConf").classList.contains("hidden") ? (document.getElementById("SeePwdConf").classList.remove("hidden")) (document.getElementById("SeePwdConf").setAttribute("name", "eye-off")) : (document.getElementById("SeePwdConf").classList.add("hidden")) (document.getElementById("SeePwdConf").setAttribute("name", "eye"));
+});
+
+function togglePasswordvisibility()
+{
+    document.getElementById("SeePwd").classList.contains("hidden") ? document.getElementById("signinPasswordInp").setAttribute("type", "password") : document.getElementById("signinPasswordInp").setAttribute("type", "text");
+    
+    document.getElementById("SeePwdReg").classList.contains("hidden") ? document.getElementById("password-containerReg").setAttribute("type", "password") : document.getElementById("password-containerReg").setAttribute("type", "text");
+    document.getElementById("SeePwdConf").classList.contains("hidden") ? document.getElementById("confirmPassword-containerReg").setAttribute("type", "password") : document.getElementById("confirmPassword-containerReg").setAttribute("type", "text");
+}
+setInterval(() => {
+    togglePasswordvisibility();
+}, 500);
